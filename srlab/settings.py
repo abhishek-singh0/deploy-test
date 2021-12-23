22f0a1ff0fc8e84f3c0bc75df07ca96f443a5549
abhishek-singh0 <abhishek.programs@gmail.com>
2021-12-23 16:25:16 +0530

    New Updates for Heroku deplyment

--- srlab/settings.py
+++ srlab/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware', # Added
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
+
 
 ]
 
