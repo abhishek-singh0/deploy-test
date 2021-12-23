50fb9aa5543f34165b0b44e218cb93148c3f23a6
abhishek-singh0 <abhishek.programs@gmail.com>
2021-12-23 15:24:18 +0530

    New Updates for Heroku deplyment

--- srlab/settings.py
+++ srlab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wgkb_qryagqxlb8l28l$ue=#z_0++-aoe137x9g(fpjvj1q$u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
